26a3864b8cab0ea578598ac6dc393b26095b4243
lihao <lihao@zego.im>
2022-12-20 15:36:25 +0800

    Replace plugin

--- src/utils/plugins.js
+++ src/utils/plugins.js
@@ -3,7 +3,7 @@ import ZegoUIKit, {
   ZegoInvitationConnectionState,
 } from '@zegocloud/zego-uikit-rn';
 import { zloginfo } from './logger';
-import ZegoUIKitSignalingPlugin from '../../example/src/plugin';
+import ZegoUIKitSignalingPlugin from '@zegocloud/zego-uikit-signaling-plugin-rn';
 
 const _appInfo = {};
 const _localUser = {};
